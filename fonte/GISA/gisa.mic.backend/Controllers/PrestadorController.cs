301dd26404456fb7add0948539160e0a739713d8
Altamir Cassiano <altamir.cassiano@energisa.com.br>
2022-01-26 18:03:16 -0300

    Iniciado implementação do MIC

---
+++ fonte/GISA/gisa.mic.backend/Controllers/PrestadorController.cs
@@ -0,0 +1,29 @@
+﻿using gisa.mic.backend.Model;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+
+namespace gisa.mic.backend.Controllers
+{
+    public class PrestadorController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IEnumerable<Prestador> Get()
+        {
+            var mock = new List<Prestador>() { 
+                new Prestador() { 
+                    Nome = "Prestador Mock 1", Descricao = "Prestador dsc Mock 1", DtCadastro = DateTime.UtcNow 
+                },
+                new Prestador() {
+                    Nome = "Prestador Mock 2", Descricao = "Prestador dsc Mock 2", DtCadastro = DateTime.UtcNow
+                }
+            };
+            return mock;
+        }
+    }
+}
---
+++ fonte/GISA/gisa.mic.backend/Model/Prestador.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace gisa.mic.backend.Model
+{
+    public class Prestador
+    {
+        public DateTime DtCadastro { get; set; }
+
+        public string Nome { get; set; }
+
+        public string Descricao { get; set; }
+        
+    }
+}
