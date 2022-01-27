55b225fd273656279d700133a4dafc7d1f233e61
Altamir Cassiano <altamir.cassiano@energisa.com.br>
2022-01-27 13:33:49 -0300

    Ajustes de imagens no artigo e inicio do MIC backend com APIRestFull

--- fonte/GISA/gisa.mic.backend/Controllers/PrestadorController.cs
+++ fonte/GISA/gisa.mic.backend/Controllers/PrestadorController.cs
@@ -5,25 +5,65 @@ using System.Collections.Generic;
 
 namespace gisa.mic.backend.Controllers
 {
+    [ApiController]
+    [Route("[controller]")]
     public class PrestadorController : Controller
     {
-        public IActionResult Index()
-        {
-            return View();
-        }
-
         [HttpGet]
         public IEnumerable<Prestador> Get()
         {
-            var mock = new List<Prestador>() { 
-                new Prestador() { 
-                    Nome = "Prestador Mock 1", Descricao = "Prestador dsc Mock 1", DtCadastro = DateTime.UtcNow 
+            var mock = new List<Prestador>() {
+                new Prestador() {
+                    Id= Guid.NewGuid().ToString(), Nome = "Prestador Mock 1", Descricao = "Prestador dsc Mock 1", DtCadastro = DateTime.UtcNow
                 },
                 new Prestador() {
-                    Nome = "Prestador Mock 2", Descricao = "Prestador dsc Mock 2", DtCadastro = DateTime.UtcNow
+                    Id= Guid.NewGuid().ToString(), Nome = "Prestador Mock 2", Descricao = "Prestador dsc Mock 2", DtCadastro = DateTime.UtcNow
                 }
             };
             return mock;
+        }
+
+        [HttpPut]
+        public ActionResult Put(Prestador prestador)
+        {
+            //if prestador is not valid
+                // return bad request
+            // dbService.Save (prestador)
+            // eventBus.Add(prestador)
+
+            return StatusCode(201);
+
+        }
+
+        [HttpPost]
+        public ActionResult Post(Prestador prestador)
+        {
+            Prestador prestadorRecuperado = GetPrestadorDB(prestador.Id);
+            if(prestadorRecuperado != null)
+                //db.update(prestadorRecuperado, prestador)
+                return StatusCode(200);
+            //if prestador is not valid
+            // return bad request
+            // dbService.Save (prestador)
+            // eventBus.Add(prestador)
+
+            return StatusCode(200);
+
+        }
+        private Prestador GetPrestadorDB(string id)
+        {
+            var prestadorMock = new Prestador() { Id = id, Nome = "Prestador Mock DB", Descricao = "DSC mock prestador db", DtCadastro = new DateTime(2022,01,01) }; //dbService busca pelo id
+            if(prestadorMock != null)
+                return prestadorMock;
+
+            return prestadorMock;
+        }
+
+        [HttpDelete]
+        public ActionResult Delete(Prestador prestador)
+        {
+            // delete db prestador
+            return StatusCode(200);
         }
     }
 }
--- fonte/GISA/gisa.mic.backend/Model/Prestador.cs
+++ fonte/GISA/gisa.mic.backend/Model/Prestador.cs
@@ -4,6 +4,7 @@ namespace gisa.mic.backend.Model
 {
     public class Prestador
     {
+        public string Id{ get; set; }
         public DateTime DtCadastro { get; set; }
 
         public string Nome { get; set; }
